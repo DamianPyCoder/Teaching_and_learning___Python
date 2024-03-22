aa12b441e75aeeda7e67640810eb24c99362d52a
Damian R. Molin <119323521+DamianPyCoder@users.noreply.github.com>
2024-03-22 23:12:22 +0100

    Add files via upload

---
+++ Youtube_DamianDevops/ClasesNumpy.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+# Array unidimensional
+# array_1d = np.array([1,2,3])
+# print("array unidimensional: ",array_1d)
+# print("")
+
+# Array multidimensional
+# array_2d = np.array([[1,2,3],[4,5,6],[7,8,9]]) 
+# print("array multidimensional:")
+# print(array_2d)
+# print("")
+
+# Propiedades
+# print("Propiedades:")
+# print("Forma del array array_2d: ", array_2d.shape)
+# print("Tamaño del array array_2d: ", array_2d.size)
+# print("Tipo de dato del array_2d: ",array_2d.dtype)
+
+# Acceso a elementos dentro del array
+# print("Primer elemento en array_1d: ", array_1d[0])
+# print("Elemento de la primera fila y la tercera columna de array_2d: ", array_2d[0,2])
+
+# Modificación de elementos en el array
+# array_1d[0] = 7
+# print(array_1d)
+# print("")
+# array_2d[0,0] = 7
+# print(array_2d)
+
+
+
+# array1 = np.array([2,4,6,8,10])
+# array2 = np.array([1,3,5,7,9])
+
+# # operaciones sencillas
+# suma_resultado=array1 + array2
+# print("Suma: ",suma_resultado)
+
+# resta_resultado=array1 - array2
+# print("Resta: ",resta_resultado)
+
+# multiplic_resultado = array1 * array2
+# print("Multiplicación: ", multiplic_resultado)
+
+# division_resultado = array2 / array1
+# print("División: ", division_resultado)
+
+# sqrt_resultado = np.sqrt(array1)
+# print("Raiz cuadrada: ", sqrt_resultado)
+
+# exponencial_resultado = np.exp(array1)
+# print("Exponencial: ",exponencial_resultado)
+
+# seno_resultado = np.sin(array1)
+# print("Seno: ",seno_resultado)
+
+# coseno_resultado = np.cos(array1)
+# print("Coseno: ",coseno_resultado)

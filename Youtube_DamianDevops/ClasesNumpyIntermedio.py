3f9f8ab195354aa48e65406d52b999108d97d2d6
Damian R. Molin <119323521+DamianPyCoder@users.noreply.github.com>
2024-03-23 23:10:44 +0100

    Add files via upload

--- Youtube_DamianDevops/ClasesNumpyIntermedio.py
+++ Youtube_DamianDevops/ClasesNumpyIntermedio.py
@@ -1,8 +1,9 @@
 import numpy as np
+import os
 
 # BROADCASTING
-# array1 = np.array([[1,2,3],[4,5,6]])
-# array2 = np.array([22,16,10])
+array1 = np.array([[1,2,3],[4,5,6]])
+array2 = np.array([22,16,10])
 # print("Broadcasting de arrays: ",array1+array2)
 
 # RESIZING
@@ -18,7 +19,34 @@ import numpy as np
 #     print(arr)
 
 # INDEXACIÓN DE ARRAYS DE ÍNDICES
-array = np.array([4,5,6,8,0,1,2,3,4,5,6,7,8])    
-indices = np.array([0,2,4])
-selected_elements = array[indices]
-print("Elementos seleccionados por índices: ", selected_elements)
+# array = np.array([4,5,6,8,0,1,2,3,4,5,6,7,8])    
+# indices = np.array([0,2,4])
+# selected_elements = array[indices]
+# print("Elementos seleccionados por índices: ", selected_elements)
+
+
+
+# ARCHIVOS
+# Cargar datos desde un archivo de texto
+# Obtener y mostrar el directorio de trabajo actual
+print("Directorio de trabajo actual:", os.getcwd())
+# Intentar cargar el archivo
+try:
+    data = np.loadtxt('palabras.txt')
+    print("Datos cargados:")
+    print(data)
+except FileNotFoundError:
+    print("No se encontró el archivo 'palabras.txt'.")
+
+# Guardar datos en un archivo
+np.savetxt('datos_guardados.txt', data)
+print("Datos guardados en el archivo: 'datos_guardados.txt'")
+
+# Guardar datos en un archivo binario
+np.save('datos_guardados.npy', data)
+print("Datos guardados en 'datos_guardados.npy'")
+
+# Cargar datos desde un archivo binario
+loaded_data = np.load('datos_guardados.npy')
+print("Datos cargados desde 'datos_guardados.npy':")
+print(loaded_data)

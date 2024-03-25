1ed09b498a9fba8aa8a61c21ab18db16208e84c3
Damian R. Molin <119323521+DamianPyCoder@users.noreply.github.com>
2024-03-25 22:23:07 +0100

    Add files via upload

--- Youtube_DamianDevops/pythonBasico.py
+++ Youtube_DamianDevops/pythonBasico.py
@@ -2,6 +2,8 @@ nombre = "Damian"
 x = 10
 y = 5
 
+# OPERACIONES
+
 # suma = x + y
 # resta = x - y
 # multiplicacion = x * y
@@ -19,7 +21,31 @@ y = 5
 
 # print(mayor, menor, igualdad, diferencia)
 
-# BUCLES FOR y WHILE
-colores = ["azul","verde","rojo"]
-for i in colores:
-    print(i)
+
+# CONDICIONALES
+# nota = 6
+# if nota == 5:
+#     print("Has aprobado")
+# elif nota == 6:
+#     print("Has sacado un bien")
+# elif nota > 6:
+#     print("Ohh genial, buena nota")
+# else:
+#     print("Suspendiste")
+# print("Final del programa")
+
+
+# BUCLES
+# colores = ["rojo","verde","azul"]
+# for i in colores:
+#     print("Mi color favorito es:")
+#     print(i)
+
+# num = 0
+# while num < 11:
+#     print(num)
+#     #num = num + 1
+#     num += 1
+    
+
+

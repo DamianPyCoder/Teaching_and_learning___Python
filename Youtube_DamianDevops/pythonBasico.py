69ceea3518007830908899fe106ceadeeb9f5757
Damian R. Molin <119323521+DamianPyCoder@users.noreply.github.com>
2024-03-25 19:32:59 +0100

    Add files via upload

---
+++ Youtube_DamianDevops/pythonBasico.py
@@ -0,0 +1,25 @@
+nombre = "Damian"
+x = 10
+y = 5
+
+# suma = x + y
+# resta = x - y
+# multiplicacion = x * y
+# division = x / y
+
+# print("La suma es ", suma)
+# print("La resta es ", resta)
+# print("La multiplicación es ", multiplicacion)
+# print("La división es ", division)
+
+# mayor = 10 > 5
+# menor = 5 < 10
+# igualdad = 10 == x
+# diferencia = x != x
+
+# print(mayor, menor, igualdad, diferencia)
+
+# BUCLES FOR y WHILE
+frutas = ["manzanas","limones","peras"]
+for i in frutas:
+    

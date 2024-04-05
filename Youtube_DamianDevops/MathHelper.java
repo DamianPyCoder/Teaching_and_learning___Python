13a7daf179f4c7b56222066c9d40ec13d4ca47f0
Damian R. Molin <119323521+DamianPyCoder@users.noreply.github.com>
2024-04-05 20:48:12 +0200

    Add files via upload

---
+++ Youtube_DamianDevops/MathHelper.java
@@ -0,0 +1,11 @@
+public class MathHelper {
+    public static int sum(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args){
+        int result = MathHelper.sum(5,3);
+        System.out.println("Suma: "+result);
+    }
+
+}
